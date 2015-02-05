c4b6c358a9a962ebfbed766de55d755f9f4328c2
Pike R. Alpha <PikeRAlpha@yahoo.com>
2015-02-05 15:06:29 +0100

    Compatibility files.

---
+++ src/include/poppack.h
@@ -0,0 +1,6 @@
+#ifndef __POP_PACK_H__
+#define __POP_PACK_H__
+
+#pragma pack()
+
+#endif // __POP_PACK_H__
---
+++ src/include/pshpack1.h
@@ -0,0 +1,6 @@
+#ifndef __PSH_PACK_H__
+#define __PSH_PACK_H__
+
+#pragma pack(1)
+
+#endif // __PSH_PACK_H__
