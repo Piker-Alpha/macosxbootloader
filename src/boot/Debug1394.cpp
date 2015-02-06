589946d9828bb2dd7e9f3462bc7a9ea041450fd7
Pike R. Alpha <PikeRAlpha@yahoo.com>
2015-02-06 01:52:50 +0100

    Fix filename, structure and GUID errors
    - whitespace only changes

--- src/boot/Debug1394.cpp
+++ src/boot/Debug1394.cpp
@@ -5,7 +5,7 @@
 //	purpose:	debug over 1394
 //********************************************************************
 
-#include "stdafx.h"
+#include "StdAfx.h"
 #include "Debug1394.h"
 
 //
@@ -155,6 +155,19 @@ typedef struct _DBG1394_DEBUG_CONFIG_INFO
 	UINT64																	ReceiveBuffer;
 }DBG1394_DEBUG_CONFIG_INFO;
 
+typedef struct _SAVED_INFO
+{
+				//
+				// saved info length
+				//
+				UINT8														CriSavedInfoLength;
+    
+				//
+				// saved crc length
+				//
+				UINT8														CriSavedCrcLength;
+}SAVED_INFO;
+
 //
 // config rom header
 //
@@ -169,18 +182,7 @@ typedef union _IEEE1394_CONFIG_ROM_INFO
 			//
 			UINT16															CriCrcValue;
 
-			struct _SAVED_INFO
-			{
-				//
-				// saved info length
-				//
-				UINT8														CriSavedInfoLength;
-
-				//
-				// saved crc length
-				//
-				UINT8														CriSavedCrcLength;
-			}SavedInfo;
+			SAVED_INFO SavedInfo;
 		};
 
 		//
@@ -308,6 +310,7 @@ STATIC UINT64 Bd1394pDataPhysicalAddress									= 0;
 // stall
 //
 #pragma optimize("", off)
+
 STATIC VOID Bd1394pStallExecution(UINT32 microseconds)
 {
 	for(UINT32 k = 0,b = 1; k < microseconds; k ++)
@@ -316,6 +319,7 @@ STATIC VOID Bd1394pStallExecution(UINT32 microseconds)
 			b																= b * (i >> k);
 	}
 }
+
 #pragma optimize("", on)
 
 //
@@ -1026,13 +1030,14 @@ EFI_STATUS Bd1394ConfigureDebuggerDevice(CHAR8 CONST* loaderOptions)
 		//
 		EFI_PCI_IO_PROTOCOL* pciIoProtocol									= nullptr;
 		EFI_HANDLE controllerHandle											= nullptr;
+
 		if(EFI_ERROR(status = BlFindPciDevice(segment, bus, device, func, PCI_CLASS_SERIAL, PCI_CLASS_SERIAL_FIREWIRE, 0x10, &pciIoProtocol, &controllerHandle)))
 			try_leave(NOTHING);
 
 		//
 		// disconnect from efi
 		//
-		EFI_GUID firewireProtocolGuid										= {0x67708aa8, 0x2079, 0x4e4f, 0xb1, 0x58, 0xb1, 0x5b, 0x1f, 0x6a, 0x6c, 0x92};
+		EFI_GUID firewireProtocolGuid										= {0x67708aa8, 0x2079, 0x4e4f, { 0xb1, 0x58, 0xb1, 0x5b, 0x1f, 0x6a, 0x6c, 0x92 } };
 		VOID* protocol														= nullptr;
 		status																= EfiBootServices->HandleProtocol(controllerHandle, &firewireProtocolGuid, &protocol);
 		CsPrintf(CHAR8_CONST_STRING("handle protocol %08x, %08x\n"), status, protocol);
