42a577abee81dab1117aa44d54fef825acba984c
Pike R. Alpha <PikeRAlpha@yahoo.com>
2015-02-06 03:13:13 +0100

    Required compatibility changes

--- src/boot/StdAfx.h
+++ src/boot/StdAfx.h
@@ -1,18 +1,32 @@
 //********************************************************************
 //	created:	4:11:2009   10:03
-//	filename: 	stdafx.h
+//	filename: 	StdAfx.h
 //	author:		tiamo
 //	purpose:	stdafx
 //********************************************************************
 
 #pragma once
 
+#ifdef __APPLE__
+  #define __leave
+  #define __try if (1)
+  #define __except(x) if (0 && (x))
+  #define __finally if (1)
+  #define _alloca(size)    MmAllocatePool(size)
+
+  #define CHAR16_STRING(S)													static_cast<CHAR16*>((VOID*)(S))
+  #define CHAR8_STRING(S)													static_cast<CHAR8*>((VOID*)(S))
+#else
+  #define CHAR16_STRING(S)													static_cast<CHAR16*>(static_cast<VOID*>(S))
+  #define CHAR8_STRING(S)													static_cast<CHAR8*>(static_cast<VOID*>(S))
+#endif
+
 #define NOTHING
 #define BOOTAPI																__cdecl
 #define CHAR8_CONST_STRING(S)												static_cast<CHAR8 CONST*>(static_cast<VOID CONST*>(S))
 #define CHAR16_CONST_STRING(S)												static_cast<CHAR16 CONST*>(static_cast<VOID CONST*>(S))
-#define CHAR8_STRING(S)														static_cast<CHAR8*>(static_cast<VOID*>(S))
-#define CHAR16_STRING(S)													static_cast<CHAR16*>(static_cast<VOID*>(S))
+
+
 #define try_leave(S)														do{S;__leave;}while(0)
 #define ARRAYSIZE(A)														(sizeof((A)) / sizeof((A)[0]))
 #define ArchConvertAddressToPointer(P,T)									((T)((UINTN)(P)))
@@ -21,7 +35,7 @@
 #define LdrStaticVirtualToPhysical(V)										((V) & (1 * 1024 * 1024 * 1024 - 1))
 #define Add2Ptr(P, O, T)													ArchConvertAddressToPointer(ArchConvertPointerToAddress(P) + (O), T)
 #define PAGE_ALIGN(A)														((A) & ~EFI_PAGE_MASK)
-#define BYTE_OFFSET(A)														((UINT32)(A) & EFI_PAGE_MASK)
+#define BYTE_OFFSET(A)														((UINTN)(A) & EFI_PAGE_MASK)
 
 #define SWAP32(V)															((((UINT32)(V) & 0xff) << 24) | (((UINT32)(V) & 0xff00) << 8) | (((UINT32)(V) & 0xff0000) >> 8) |  (((UINT32)(V) & 0xff000000) >> 24))
 #define SWAP_BE32_TO_HOST													SWAP32
