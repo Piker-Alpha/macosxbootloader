8aba2e77a647f3bc6fc6f9b841ab652a301706b3
Pike R. Alpha <PikeRAlpha@yahoo.com>
2015-03-11 10:52:30 +0100

    Whitespace only changes

--- src/boot/x86/ThunkCode32.asm
+++ src/boot/x86/ThunkCode32.asm
@@ -14,18 +14,18 @@
 section .data
 
 %ifdef __DEBUG__
-%define									_ThunkCode64.dat	"../../../temp/x86/Debug/boot/ThunkCode64.dat"
+%define _ThunkCode64.dat			"../../../temp/x86/Debug/boot/ThunkCode64.dat"
 %else
-%define									_ThunkCode64.dat	"../../../temp/x86/Release/boot/ThunkCode64.dat"
+%define _ThunkCode64.dat			"../../../temp/x86/Release/boot/ThunkCode64.dat"
 %endif
 
 %else
 section .rdata
-%define									_ThunkCode64.dat	"ThunkCode64.dat"
+%define _ThunkCode64.dat			"ThunkCode64.dat"
 %endif
 
-										align 16
+									align 16
 
 PUBLIC_SYMBOL ?ArchThunk64BufferStart@@3PAEA
-										incbin _ThunkCode64.dat
+									incbin				_ThunkCode64.dat
 PUBLIC_SYMBOL ?ArchThunk64BufferEnd@@3PAEA
